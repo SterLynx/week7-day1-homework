fb5583b7b155e580698757ad211d770a41934ce3
Sterling Meggs <sterlynx@gmail.com>
2023-10-31 21:14:49 -0700

    Homework

--- scratch.js
+++ scratch.js
@@ -1,1 +1,6 @@
-var coupoundString = console.log(hereinbefore.slice(-5))
+var dogNames = ["Max","Fido","Gizmo","Nala"]
+
+//Test Cases
+var testString1 = "Hello, my dog is Max, and they have purple eyes!"
+//Expect ['Max']
+console.log(dogNames.filter( (dog) => testString1.includes(dog) ))
